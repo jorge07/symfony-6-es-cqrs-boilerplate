3fed81a334d05057ab691091f6a476e5d106382b
Jorge Arco <jorge.arcoma@gmail.com>
2018-02-05 18:03:03 +0100

    Update CreateUserHandler.php

--- src/Application/Command/User/Create/CreateUserHandler.php
+++ src/Application/Command/User/Create/CreateUserHandler.php
@@ -3,36 +3,22 @@
 namespace App\Application\Command\User\Create;
 
 use App\Application\Command\CommandHandlerInterface;
-use App\Domain\User\Exception\EmailAlreadyExistException;
 use App\Domain\User\Factory\UserFactory;
-use App\Domain\User\Repository\UserCollectionInterface;
 use App\Domain\User\Repository\UserRepositoryInterface;
-use App\Domain\User\ValueObject\Email;
 
 class CreateUserHandler implements CommandHandlerInterface
 {
     public function __invoke(CreateUserCommand $command): void
     {
-        $this->checkEmail($command->credentials->email);
-
         $aggregateRoot = $this->userFactory->register($command->uuid, $command->credentials);
 
         $this->userRepository->store($aggregateRoot);
     }
 
-    private function checkEmail(Email $email): void
-    {
-        if ($this->userCollection->existsEmail($email)) {
-
-            throw new EmailAlreadyExistException();
-        }
-    }
-
-    public function __construct(UserFactory $userFactory, UserRepositoryInterface $userRepository, UserCollectionInterface $userCollection)
+    public function __construct(UserFactory $userFactory, UserRepositoryInterface $userRepository)
     {
         $this->userFactory = $userFactory;
         $this->userRepository = $userRepository;
-        $this->userCollection = $userCollection;
     }
 
     /**
@@ -44,9 +30,4 @@ class CreateUserHandler implements CommandHandlerInterface
      * @var UserRepositoryInterface
      */
     private $userRepository;
-
-    /**
-     * @var UserCollectionInterface
-     */
-    private $userCollection;
 }
