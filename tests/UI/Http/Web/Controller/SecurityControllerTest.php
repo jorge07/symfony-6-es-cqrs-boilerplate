55594cbbdab5d2fc640a27a5ddcba4b7033c95bf
Luis Tacón <luis.t@macnificos.com>
2018-06-01 23:36:24 +0200

    New test for UserSignedIn event and test wrong credentials login.

--- tests/UI/Http/Web/Controller/SecurityControllerTest.php
+++ tests/UI/Http/Web/Controller/SecurityControllerTest.php
@@ -26,13 +26,13 @@ class SecurityControllerTest extends WebTestCase
 
         $form->get('_email')->setValue('jorge@gmail.com');
         $form->get('_password')->setValue('crqs-demo');
-        
+
         $client->submit($form);
 
         $crawler = $client->followRedirect();
 
-        self::assertEquals('/profile', parse_url($crawler->getUri(), PHP_URL_PATH));
-        self::assertEquals(1, $crawler->filter('html:contains("Hi jorge@gmail.com")')->count());
+        self::assertSame('/profile', parse_url($crawler->getUri(), PHP_URL_PATH));
+        self::assertSame(1, $crawler->filter('html:contains("Hi jorge@gmail.com")')->count());
     }
 
     /**
@@ -67,6 +67,28 @@ class SecurityControllerTest extends WebTestCase
 
         $crawler = $client->followRedirect();
         self::assertEquals('/sign-in', parse_url($crawler->getUri(), PHP_URL_PATH));
+    }
+
+    /**
+     * @test
+     *
+     * @group e2e
+     */
+    public function login_should_display_an_error_when_bad_credentials()
+    {
+        $client = self::createClient();
+
+        $crawler = $client->request('GET', '/sign-in');
+
+        $form = $crawler->selectButton('Sign in')->form();
+
+        $form->get('_email')->setValue('an@email.com');
+        $form->get('_password')->setValue('password-so-safe');
+
+        $client->submit($form);
+
+        $crawler = $client->followRedirect();
+        self::assertEquals(1, $crawler->filter('html:contains("An authentication exception occurred.")')->count());
     }
 
     private function createUser(string $email, string $password = 'crqs-demo'): Crawler
