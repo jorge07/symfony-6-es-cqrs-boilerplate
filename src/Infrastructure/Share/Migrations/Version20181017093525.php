a31e4f10fdcaeafc7ab52a1a9889899fccc23c3a
alekseytupichenkov <aleksey.tupichenkov@eglobal-group.com>
2018-10-17 12:48:39 +0300

    cs

--- src/Infrastructure/Share/Migrations/Version20181017093525.php
+++ src/Infrastructure/Share/Migrations/Version20181017093525.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -10,18 +12,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20181017093525 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE users CHANGE uuid uuid BINARY(16) NOT NULL COMMENT \'(DC2Type:App\\\\Domain\\\\User\\\\ValueObject\\\\Uuid)\', CHANGE credentials_email credentials_email VARCHAR(255) NOT NULL COMMENT \'(DC2Type:App\\\\Domain\\\\User\\\\ValueObject\\\\Email)\', CHANGE credentials_password credentials_password VARCHAR(255) NOT NULL COMMENT \'(DC2Type:App\\\\Domain\\\\User\\\\ValueObject\\\\Auth\\\\HashedPassword)\'');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE users CHANGE uuid uuid BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid_binary)\', CHANGE credentials_email credentials_email VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE credentials_password credentials_password VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
     }
