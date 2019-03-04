df54f04c7efb43c3930b70c468beaa803d12bfe6
Jorge Arco <jorge.arcoma@gmail.com>
2019-03-04 16:44:29 +0100

    Refactor

--- src/Infrastructure/User/Specification/UniqueEmailSpecification.php
+++ src/Infrastructure/User/Specification/UniqueEmailSpecification.php
@@ -4,19 +4,33 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\User\Specification;
 
+use App\Domain\Shared\Specification\AbstractSpecification;
 use App\Domain\User\Exception\EmailAlreadyExistException;
 use App\Domain\User\Repository\CheckUserByEmailInterface;
 use App\Domain\User\Specification\UniqueEmailSpecificationInterface;
 use App\Domain\User\ValueObject\Email;
+use Doctrine\ORM\NonUniqueResultException;
 
-final class UniqueEmailSpecification implements UniqueEmailSpecificationInterface
+final class UniqueEmailSpecification extends AbstractSpecification implements UniqueEmailSpecificationInterface
 {
     /**
      * @throws EmailAlreadyExistException
      */
     public function isUnique(Email $email): bool
     {
-        if ($this->checkUserByEmail->existsEmail($email)) {
+        return $this->isSatisfiedBy($email);
+    }
+
+    /**
+     * @param Email $value
+     */
+    public function isSatisfiedBy($value): bool
+    {
+        try {
+            if ($this->checkUserByEmail->existsEmail($value)) {
+                throw new EmailAlreadyExistException();
+            }
+        } catch (NonUniqueResultException $e) {
             throw new EmailAlreadyExistException();
         }
 
