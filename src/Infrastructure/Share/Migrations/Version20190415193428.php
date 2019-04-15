b20d8c7a2de103ad8d077a1afd199eb9159fd8b3
cristianbargansmarin <cbargans@gmail.com>
2019-04-15 21:38:33 +0200

    Create a migration instead of modify the original one

--- src/Infrastructure/Share/Migrations/Version20190415193428.php
+++ src/Infrastructure/Share/Migrations/Version20190415193428.php
@@ -10,14 +10,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180204151052 extends AbstractMigration
+class Version20190415193428 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE users (uuid BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid_binary)\', credentials_email VARCHAR(255) NOT NULL, credentials_password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9299C9369 (credentials_email), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE users ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:domain_datetime_immutable)\', ADD updated_at DATETIME NOT NULL COMMENT \'(DC2Type:domain_datetime_immutable)\'');
     }
 
     public function down(Schema $schema): void
@@ -25,6 +25,6 @@ class Version20180204151052 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE users');
+        $this->addSql('ALTER TABLE users DROP created_at, DROP updated_at');
     }
 }
