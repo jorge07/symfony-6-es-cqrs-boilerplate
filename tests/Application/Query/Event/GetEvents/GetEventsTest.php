76443efe6840925b3aa7e89e17a56d6feb462745
Jorge Arco <jorge.arcoma@gmail.com>
2019-06-02 17:37:49 +0200

    SF 4.2 and PHPunit 8 upgrade patches

--- tests/Application/Query/Event/GetEvents/GetEventsTest.php
+++ tests/Application/Query/Event/GetEvents/GetEventsTest.php
@@ -33,7 +33,7 @@ final class GetEventsTest extends ApplicationTestCase
      * @throws \Exception
      * @throws \Assert\AssertionFailedException
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
@@ -62,7 +62,7 @@ final class GetEventsTest extends ApplicationTestCase
         $eventReadStore->refresh();
     }
 
-    protected function tearDown()
+    protected function tearDown(): void
     {
         /** @var EventElasticRepository $eventReadStore */
         $eventReadStore = $this->service('events_repository');
