07999d206abca29c092611d8c0137d11e1e426d1
Luis Tacon <luis@tacon.eu>
2019-06-07 15:37:38 +0200

    Add missing qualifier.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -11,7 +11,7 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
     $_ENV += $env;
 } elseif (!isset($_SERVER['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
-        throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+        throw new \RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
     }
     // load all the .env files
     (new Dotenv(true))->loadEnv(dirname(__DIR__) . '/.env');
