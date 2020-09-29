9599220c34279d70e61ca7285c161589c01b91db
Jorge Arco <jorge.arcoma@gmail.com>
2020-09-29 11:40:44 +0200

    upgrade sf and phpstan

---
+++ src/Infrastructure/.preload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(__DIR__.'/../../var/cache/dev/App_Infrastructure_KernelDevDebugContainer.preload.php')) {
+    require __DIR__.'/../../var/cache/dev/App_Infrastructure_KernelDevDebugContainer.preload.php';
+}
