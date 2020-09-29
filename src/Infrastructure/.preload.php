8a62667f88dc419f5f88f904afa4e71c17268a5d
Jorge Arco <jorge.arcoma@gmail.com>
2020-09-29 11:58:37 +0200

    Fix deprecation

--- src/Infrastructure/.preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(__DIR__.'/../../var/cache/dev/App_Infrastructure_KernelDevDebugContainer.preload.php')) {
-    require __DIR__.'/../../var/cache/dev/App_Infrastructure_KernelDevDebugContainer.preload.php';
-}
