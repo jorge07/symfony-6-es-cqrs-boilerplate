5bdc8ffb876ccfaf952c7dc1d272bc6f35304bbe
Jorge Arco <jorge.arcoma@gmail.com>
2026-02-06 11:23:27 +0100

    Revert "Remove unused CreateUserTrait"
    This reverts commit 742fde9977c302e3272fee0e661a9b19573341ed.

---
+++ tests/UI/Http/Web/Controller/CreateUserTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\App\UI\Http\Web\Controller;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+trait CreateUserTrait
+{
+    private function createUser(string $email, string $password = 'crqs-demo'): KernelBrowser
+    {
+        self::ensureKernelShutdown();
+        $client = self::createClient();
+
+        $crawler = $client->request('GET', '/sign-up');
+
+        $form = $crawler->selectButton('Send')->form();
+
+        $form->get('email')->setValue($email);
+        $form->get('password')->setValue($password);
+
+        $client->submit($form);
+
+        return $client;
+    }
+}
