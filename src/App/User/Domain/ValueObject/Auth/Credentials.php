a656e9aa9e3618d5291aecff17f05ea10981d7b2
Jorge Arco <jorge.arcoma@gmail.com>
2026-02-06 11:23:27 +0100

    Revert "Convert Doctrine XML mappings to PHP attributes"
    This reverts commit 1b7ce3cbc69b50a1ff672210da07b65202433f13.

--- src/App/User/Domain/ValueObject/Auth/Credentials.php
+++ src/App/User/Domain/ValueObject/Auth/Credentials.php
@@ -5,20 +5,10 @@ declare(strict_types=1);
 namespace App\User\Domain\ValueObject\Auth;
 
 use App\User\Domain\ValueObject\Email;
-use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Embeddable]
 class Credentials
 {
-    #[ORM\Column(name: 'email', type: 'email', unique: true)]
-    public Email $email;
-
-    #[ORM\Column(name: 'password', type: 'hashed_password')]
-    public HashedPassword $password;
-
-    public function __construct(Email $email, HashedPassword $password)
+    public function __construct(public Email $email, public HashedPassword $password)
     {
-        $this->email = $email;
-        $this->password = $password;
     }
 }
